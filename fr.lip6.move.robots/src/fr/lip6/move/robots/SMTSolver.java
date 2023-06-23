1987639a54434066144a4afa3d878e584a3198cb
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-06-23 16:11:08 +0200

    enlarged rigid scenario + traces/stats

--- fr.lip6.move.robots/src/fr/lip6/move/robots/SMTSolver.java
+++ fr.lip6.move.robots/src/fr/lip6/move/robots/SMTSolver.java
@@ -26,12 +26,22 @@ public class SMTSolver {
 	private org.smtlib.SMT smt = new SMT();
 	private ISolver solver = SMTUtils.initSolver(smt,"QF_LIA",7200,7200);
 	private int [] fixedStrat;
+	private int nbSym=0;
+	private Statistics stats;
+	private boolean enlarge ;
 
-	public void declareVariables(List<int[]>[] observations, Map<String, Integer> obsMap) {
+	public SMTSolver(Statistics stats, boolean enlarge) {
+		this.stats = stats;
+		this.enlarge = enlarge;
+	}
+
+	public void declareVariables(List<int[]>[] observations, Map<String, Integer> obsMap, int totalSym) {
 		Script script = new Script();
 		IFactory ef = smt.smtConfig.exprFactory;
 		org.smtlib.ISort.IApplication ints = smt.smtConfig.sortFactory.createSortExpression(ef.symbol("Int"));
-
+		this.nbSym = totalSym;
+		
+		
 		for (int[] obs : observations[0]) {
 			ISymbol si = ef.symbol("s"+ obsMap.get(Arrays.toString(obs)));
 			script.add(new org.smtlib.command.C_declare_fun(
@@ -56,19 +66,47 @@ public class SMTSolver {
 	}
 
 	public void addConstraint(Set<Integer> trace, int [] strat) {
+		long time = System.currentTimeMillis();
 		IFactory ef = smt.smtConfig.exprFactory;
-
+		Script script = new Script();
 		List<IExpr> expr = new ArrayList<>();
 		for (Integer obs : trace) {
+			if (fixedStrat[obs] != -1 && enlarge) {
+				org.smtlib.ISort.IApplication ints = smt.smtConfig.sortFactory.createSortExpression(ef.symbol("Int"));
+				// introduce a variable and relax
+				if (obs < nbSym) {
+					ISymbol si = ef.symbol("s"+ obs);
+					script.add(new org.smtlib.command.C_declare_fun(
+							si,
+							Collections.emptyList(),
+							ints								
+							));
+					script.add(new C_assert(ef.fcn(ef.symbol(">="), si, ef.numeral(Action.MOVE.ordinal()))));
+					script.add(new C_assert(ef.fcn(ef.symbol("<="), si, ef.numeral(Action.STAY.ordinal()))));
+				} else {
+					ISymbol si = ef.symbol("s"+ obs);
+					script.add(new org.smtlib.command.C_declare_fun(
+							si,
+							Collections.emptyList(),
+							ints								
+							));
+					script.add(new C_assert(ef.fcn(ef.symbol(">="), si, ef.numeral(Action.STAY.ordinal()))));
+					script.add(new C_assert(ef.fcn(ef.symbol("<="), si, ef.numeral(Action.RIGHT.ordinal()))));
+				}
+				fixedStrat[obs] = -1;
+				System.out.println("Relaxing strategy for "+ obs);
+			}
 			expr.add(ef.fcn(ef.symbol("not"),ef.fcn(ef.symbol("="), ef.symbol("s"+obs), ef.numeral(strat[obs]))));
 		}
-		Script s = new Script();
-		s.add(new C_assert(SMTUtils.makeOr(expr)));
+		
+		script.add(new C_assert(SMTUtils.makeOr(expr)));
 
-		SMTUtils.execAndCheckResult(s, solver);
+		SMTUtils.execAndCheckResult(script, solver);
+		stats.reportTime(Tool.SMT, System.currentTimeMillis() - time);
 	}
 
 	public int[] readStrategy() throws NoStrategyExistsException {
+		long time = System.currentTimeMillis();
 		String response = SMTUtils.checkSat(solver);
 		if ("unsat".equals(response)) {
 			throw new NoStrategyExistsException();
@@ -92,6 +130,7 @@ public class SMTSolver {
 					}
 				}
 			}
+			stats.reportTime(Tool.SMT, System.currentTimeMillis() - time);
 			return strat;
 		} else {
 			System.err.println("Failed to check sat, obtained " + response);
@@ -101,7 +140,9 @@ public class SMTSolver {
 
 	public void setRigidStrategy(Map<String, Action> rigid, Map<String, Integer> obsMap) {
 		fixedStrat = new int[obsMap.size()];
-		
+		for (int i=0; i < fixedStrat.length ; i++) {
+			fixedStrat[i] = -1;
+		}
 		// Iterate through each observation-action pair in the rigid map
 		for (Entry<String, Action> entry : rigid.entrySet()) {
 			String obsStr = entry.getKey();
